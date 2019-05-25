f6a2126f56f880bb791969582b567f1ea483bf97
Caleb Seeling <primocaleb@gmail.com>
2019-05-25 11:08:15 +0200

    Uhr

--- app/src/main/java/com/example/marlonscheer/projectv/HomeActivity.java
+++ app/src/main/java/com/example/marlonscheer/projectv/HomeActivity.java
@@ -3,8 +3,15 @@ package com.example.marlonscheer.projectv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import java.util.Calendar;
+
+
+import java.sql.Time;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,6 +25,10 @@ public class HomeActivity extends AppCompatActivity {
         wv = (WebView) findViewById(R.id.webView);
 
         wv.loadUrl("https://github.com");
+        RelativeLayout v = (RelativeLayout)  findViewById(R.id.nav);
+
+        TextView s = (TextView) v.findViewById(R.id.date_text);
+        s.setText(Calendar.getInstance().getTime().toString());
 
 
 
