6a33e07ed3c0fc3e9c70d30512efb2f038dc0ac7
R3DX03 <marlon.scheer@t-online.de>
2019-05-25 08:52:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marlonscheer/projectv/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.marlonscheer.projectv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String uname = "realaxelvoss";
+        String passwd = "dsgvo";
+    }
+}
