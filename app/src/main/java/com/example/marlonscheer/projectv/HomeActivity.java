69c4cf44ba7ace7dc891b9a8edbab6f21c2acca4
Caleb Seeling <primocaleb@gmail.com>
2019-05-25 14:18:12 +0200

    PUSH....PUSH IT REAL GOOD

--- app/src/main/java/com/example/marlonscheer/projectv/HomeActivity.java
+++ app/src/main/java/com/example/marlonscheer/projectv/HomeActivity.java
@@ -34,10 +34,12 @@ import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
 import java.sql.Time;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -54,39 +56,16 @@ public class HomeActivity extends AppCompatActivity {
         RelativeLayout v = (RelativeLayout)  findViewById(R.id.nav);
 
         TextView s = (TextView) v.findViewById(R.id.date_text);
-        s.setText( DateFormat.getTimeInstance().toString());
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable=true;
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.mipmap.face,
-                options);
-        Paint myRectPaint = new Paint();
-        myRectPaint.setStrokeWidth(5);
-        myRectPaint.setColor(Color.RED);
-        myRectPaint.setStyle(Paint.Style.STROKE);
-        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
-        Canvas tempCanvas = new Canvas(tempBitmap);
-        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
-        FaceDetector faceDetector = new
-                FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
-                .build();
-        if(!faceDetector.isOperational()){
-            new AlertDialog.Builder(v.getContext()).setMessage("Could not set up the face detector!").show();
-            return;
-        }
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Face> faces = faceDetector.detect(frame);
-        for(int i=0; i<faces.size(); i++) {
-            Face thisFace = faces.valueAt(i);
-            float x1 = thisFace.getPosition().x;
-            float y1 = thisFace.getPosition().y;
-            float x2 = x1 + thisFace.getWidth();
-            float y2 = y1 + thisFace.getHeight();
-            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-        }
-        myImageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        //get current date time with Date()
+        Date date = new Date();
+
+
+        //get current date time with Calendar()
+        Calendar cal = Calendar.getInstance();
+
+        s.setText(dateFormat.format(cal.getTime()) );
 
 
 
