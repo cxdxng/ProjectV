93374b52ca4179f304cafd475a70e010897af5de
R3DX03 <marlon.scheer@t-online.de>
2019-05-25 14:21:24 +0200

    BugFixes agaaaaaaain

---
+++ app/src/main/java/com/example/marlonscheer/projectv/SensorActivity.java
@@ -0,0 +1,14 @@
+package com.example.marlonscheer.projectv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+    }
+}
