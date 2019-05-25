9723c84c49fda12d1e45e5a7ddbd0a3c730f7532
R3DX03 <marlon.scheer@t-online.de>
2019-05-25 14:25:45 +0200

    Stepper Backend

--- app/src/main/java/com/example/marlonscheer/projectv/SensorActivity.java
+++ app/src/main/java/com/example/marlonscheer/projectv/SensorActivity.java
@@ -3,12 +3,56 @@ package com.example.marlonscheer.projectv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
 
 public class SensorActivity extends AppCompatActivity {
+
+    Button up;
+    Button down;
+    Button left;
+    Button right;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        up = (Button) findViewById(R.id.arrowUp);
+        down = (Button) findViewById(R.id.arrowDown);
+        left = (Button) findViewById(R.id.arrowLeft);
+        right = (Button) findViewById(R.id.arrowRight);
+
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
