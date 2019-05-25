241a78414ce7b9abbdece35d8d820ec8e106da8d
R3DX03 <marlon.scheer@t-online.de>
2019-05-25 16:44:58 +0200

    ONCLICK

--- app/src/main/java/com/example/marlonscheer/projectv/SensorActivity.java
+++ app/src/main/java/com/example/marlonscheer/projectv/SensorActivity.java
@@ -5,15 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.sql.BatchUpdateException;
 
 public class SensorActivity extends AppCompatActivity {
 
-    Button up;
-    Button down;
-    Button left;
-    Button right;
+
 
 
     @Override
@@ -21,38 +20,7 @@ public class SensorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
 
-        up = (Button) findViewById(R.id.arrowUp);
-        down = (Button) findViewById(R.id.arrowDown);
-        left = (Button) findViewById(R.id.arrowLeft);
-        right = (Button) findViewById(R.id.arrowRight);
 
-        up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        down.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-            }
-        });
 
     }
 }
